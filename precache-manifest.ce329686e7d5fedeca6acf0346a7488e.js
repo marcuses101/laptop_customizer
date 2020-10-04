f0ec4888131c46864da9202f97d16f69cdfd5540
marcuses101 <mnjconnolly@gmail.com>
2020-10-03 20:17:17 -0400

    Updates

---
+++ precache-manifest.ce329686e7d5fedeca6acf0346a7488e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f9fae165c57063ac40963e61fb16e47",
+    "url": "/laptop_customizer/index.html"
+  },
+  {
+    "revision": "f0bd8b672d1bc78846ca",
+    "url": "/laptop_customizer/static/css/main.42a420bb.chunk.css"
+  },
+  {
+    "revision": "a72f63710881fe35cbbd",
+    "url": "/laptop_customizer/static/js/2.36def64e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/laptop_customizer/static/js/2.36def64e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0bd8b672d1bc78846ca",
+    "url": "/laptop_customizer/static/js/main.c2cfded9.chunk.js"
+  },
+  {
+    "revision": "cea92e930ef750dc4fc0",
+    "url": "/laptop_customizer/static/js/runtime-main.cff6aa8b.js"
+  }
+]);
